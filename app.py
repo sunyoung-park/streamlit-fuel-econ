3e620b87d53b62758279093e6cd9ecdadb5320e5
sunyoung-park <young170627@gmail.com>
2023-11-24 11:48:53 +0900

    eeeee

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 def main():
-    st.title('자동차 데이터 분석 ~!!!흑흑ㅎddmrgmrr')
-    st.subheader('차트 보기')
+    st.title('자동차 데이터 분석 ~!!!')
+    st.subheader('미국에서 운행중인 자동차 데이터')
     df =pd.read_csv('./data/fuel_econ.csv')
 
     if st.checkbox('데이터 프레임 보기') : 
